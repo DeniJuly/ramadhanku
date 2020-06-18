0b37875b3b18eae2e13f3991be8eb2b8da551ae9
DeniJuliSetiawan112 <denijuli.setiawan@gmail.com>
2020-06-18 21:52:26 +0700

    change config url

--- src/config/url.js
+++ src/config/url.js
@@ -1,7 +1,7 @@
 export default{
-    api: 'http://192.168.43.92:8000/api/',
-    fotoProfile: 'http://192.168.43.92:8000/img/foto-profile/',
-    ilustrasi: 'http://192.168.43.92:8000/img/surat/',
+    api: 'https://ramadhankuapi.iamdeni.com/api/',
+    fotoProfile: 'https://ramadhankuapi.iamdeni.com/img/foto-profile/',
+    ilustrasi: 'https://ramadhankuapi.iamdeni.com/img/surat/',
     apiKota: 'https://api.banghasan.com/sholat/format/json/kota/kode/',
     apiSurat: 'https://api.banghasan.com/quran/format/json/surat',
     apiShalat: 'https://api.banghasan.com/sholat/format/json/jadwal/kota/',
